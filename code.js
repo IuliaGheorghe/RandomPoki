d85ea8d3ba920d7df86eda2d4e17d2a0a82097b0
IuliaGheorghe <iulia.gheorghe25@192-168-0-105.rdsnet.ro>
2021-06-16 14:49:54 +0300

    Small Project - Random Pokemon Generator

---
+++ code.js
@@ -0,0 +1,101 @@
+var pokemons=[
+
+{
+    name: "Pikachu",
+    height: "0.4 m",
+    weight: "5.9 kg",
+    source: "https://cdn.pixabay.com/photo/2020/08/29/16/08/pikachu-5527377_960_720.jpg",
+    sourceBg: 'url("https://cdn.pixabay.com/photo/2020/08/29/16/08/pikachu-5527377_960_720.jpg")',
+},
+
+{
+	name: "Eelektrik",
+	height: "1.2 m",
+	weight: "22.0 kg",
+	source: "https://cdn2.bulbagarden.net/upload/6/6c/604Eelektross.png",
+	sourceBg: 'url("https://cdn2.bulbagarden.net/upload/6/6c/604Eelektross.png")',
+},
+
+{
+    name: "Meloetta Pirouette",
+    height: "0.6 m",
+    weight: "6.5 kg",
+    source: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/be718605-eac9-40a7-bccd-dea055256f78/d58nbax-e7921d1f-0488-4ce7-8695-8c33d07eae07.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JlNzE4NjA1LWVhYzktNDBhNy1iY2NkLWRlYTA1NTI1NmY3OFwvZDU4bmJheC1lNzkyMWQxZi0wNDg4LTRjZTctODY5NS04YzMzZDA3ZWFlMDcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.QGnWbqJigDGD6xhi5vJgy7DRGuB3LE52CBCH1IV67t4",
+    sourceBg: 'url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/be718605-eac9-40a7-bccd-dea055256f78/d58nbax-e7921d1f-0488-4ce7-8695-8c33d07eae07.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JlNzE4NjA1LWVhYzktNDBhNy1iY2NkLWRlYTA1NTI1NmY3OFwvZDU4bmJheC1lNzkyMWQxZi0wNDg4LTRjZTctODY5NS04YzMzZDA3ZWFlMDcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.QGnWbqJigDGD6xhi5vJgy7DRGuB3LE52CBCH1IV67t4")',
+},
+
+{
+    name: "Probopass",
+    height: "1.4 m",
+    weight: "340.0 kg",
+    source: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgO9EcunuAvegu4_hJfY1NYsDAREBkTmpFLA&usqp=CAU",
+    sourceBg: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgO9EcunuAvegu4_hJfY1NYsDAREBkTmpFLA&usqp=CAU")',
+},
+
+{
+    name: "Zangoose",
+    height: "1.3 m",
+    weight: "40.3 kg",
+    source: "https://img.pokemondb.net/artwork/large/zangoose.jpg",
+    sourceBg: 'url("https://img.pokemondb.net/artwork/large/zangoose.jpg")',
+},
+
+{
+    name: "Musharna",
+    height: "1.1 m",
+    weight: "60.5 kg",
+    source: "http://static.pokemonpets.com/images/monsters-images-800-800/518-Musharna.png",
+    sourceBg: 'url("http://static.pokemonpets.com/images/monsters-images-800-800/518-Musharna.png")',
+},
+
+{
+    name: "Munna",
+    height: "0.6 m",
+    weight: "23.3 kg",
+    source: "https://cdn2.bulbagarden.net/upload/thumb/6/61/517Munna.png/1200px-517Munna.png",
+    sourceBg: 'url("https://cdn2.bulbagarden.net/upload/thumb/6/61/517Munna.png/1200px-517Munna.png")',
+},
+
+{
+    name: "Snubbull",
+    height: "0.6 m",
+    weight: "7.8 kg",
+    source: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/209.png",
+    sourceBg: 'url("https://assets.pokemon.com/assets/cms2/img/pokedex/full/209.png")',
+},
+
+{
+    name: "Cottonee",
+    height: "0.3 m",
+    weight: "0.6 kg",
+    source: "https://cdn2.bulbagarden.net/upload/thumb/a/a2/547Whimsicott.png/1200px-547Whimsicott.png",
+    sourceBg: 'url("https://cdn2.bulbagarden.net/upload/thumb/a/a2/547Whimsicott.png/1200px-547Whimsicott.png")',
+},
+
+{
+    name: "Drifloon",
+    height: "0.4 m",
+    weight: "1.2 kg",
+    source: "http://static.pokemonpets.com/images/monsters-images-800-800/425-Drifloon.png",
+    sourceBg: 'url("http://static.pokemonpets.com/images/monsters-images-800-800/425-Drifloon.png")',
+}
+
+]
+
+let oldIndex=0;
+
+function chooseNew(){
+
+    
+	let index=Math.floor(Math.random() * pokemons.length);
+
+	while(index==oldIndex) index=Math.floor(Math.random() * pokemons.length)
+
+		oldIndex=index;
+	
+	document.getElementsByTagName('h1')[0].innerHTML=pokemons[index].name;
+	document.getElementsByTagName('h3')[0].innerHTML="Height: " + pokemons[index].height;
+	document.getElementsByTagName('h3')[1].innerHTML="Weight: " + pokemons[index].weight;
+	document.getElementsByTagName('img')[0].src=pokemons[index].source;
+	document.getElementsByTagName('body')[0].style.backgroundImage=pokemons[index].sourceBg;
+}
